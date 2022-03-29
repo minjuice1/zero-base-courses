aa33d9e1ec8e6c26c9ad98edf448ffa88ae60221
minjuice1 <minjuice93@gmail.com>
2022-03-29 19:33:31 +0900

    algorithm : code runner 설치 및 실행위해 base 코드 작성

---
+++ algorithm/main.js
@@ -0,0 +1,12 @@
+// const fs = require("fs");
+// const input = fs.readFileSync("./algorithm/input.txt", "utf8");
+
+// console.log(input);
+
+let 안녕 = "하세요";
+let hi = "hello";
+let HI = "HeLLO";
+
+console.log(안녕);
+console.log(hi);
+console.log(HI);
