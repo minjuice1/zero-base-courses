70c585c826a5cce8d55b732cab0931e995867f2d
minjuice1 <minjuice93@gmail.com>
2022-04-22 18:14:16 +0900

    🎨 추가, case3-drumkit 구현

--- js-projects/case3-drumkit/script.js
+++ js-projects/case3-drumkit/script.js
@@ -9,6 +9,7 @@
     return document.querySelectorAll(target)
   }
 
+  const keys = Array.from(getAll('.key'))
   const soundsRoot = 'assets/sounds/'
   const drumSounds = [
     { key: 81, sound: 'clap.wav' },
@@ -21,4 +22,49 @@
     { key: 88, sound: 'snare.wav' },
     { key: 67, sound: 'tom.wav' },
   ]
+
+  const getAudioElement = (idx) => {
+    const audio = document.createElement('audio')
+    audio.dataset.key = drumSounds[idx].key
+    audio.src = soundsRoot + drumSounds[idx].sound
+    return audio
+  }
+
+  const onKeydown = (e) => {
+    const keycode = e.which
+    // const keycode = e.keycode
+    playsound(keycode)
+  }
+
+  const onMousedown = (e) => {
+    const keycode = e.target.dataset.key
+    playsound(keycode)
+  }
+
+  const playsound = (keycode) => {
+    const $audio = get(`audio[data-key="${keycode}"`)
+    const $key = get(`div[data-key="${keycode}"`)
+    if ($key && $audio) {
+      $key.classList.add('playing')
+      $audio.currentTime = 0
+      $audio.play()
+    }
+  }
+
+  const onTransitionEnd = (e) => {
+    if (e.propertyName === 'transform') {
+      e.target.classList.remove('playing')
+    }
+  }
+  const init = () => {
+    window.addEventListener('keydown', onKeydown)
+    keys.forEach((key, idx) => {
+      key.dataset.key = drumSounds[idx].key
+      const audio = getAudioElement(idx)
+      key.appendChild(audio)
+      key.addEventListener('click', onMousedown)
+      key.addEventListener('transitionend', onTransitionEnd)
+    })
+  }
+  init()
 })()
