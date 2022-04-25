30493d7491fe5943ed750c49289e486d0126d0ce
minjuice1 <minjuice93@gmail.com>
2022-04-25 17:18:39 +0900

    🐎 추가,  성능 향상을 위해 throttle 적용

--- js-projects/case4-progress_bar/script.js
+++ js-projects/case4-progress_bar/script.js
@@ -5,16 +5,28 @@
     return document.querySelector(target)
   }
 
-  const $progressBar = get('.progress-bar')
+  let timerId
+  const $progress_bar = get('.progress-bar')
+
+  const throttle = (callback, time) => {
+    if (timerId) return
+
+    timerId = setTimeout(() => {
+      callback()
+      timerId = undefined
+    }, time)
+  }
 
   const onScroll = () => {
     const scrollTop = document.documentElement.scrollTop
     const height =
       document.documentElement.scrollHeight -
       document.documentElement.clientHeight
-    const widthScroll = (scrollTop / height) * 100
-    $progressBar.style.width = widthScroll + '%'
+    const scrollWidth = (scrollTop / height) * 100
+    $progress_bar.style.width = scrollWidth + '%'
   }
 
-  window.addEventListener('scroll', onScroll)
+  window.addEventListener('scroll', () => {
+    throttle(onScroll, 100)
+  })
 })()
