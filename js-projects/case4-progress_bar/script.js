9a6593b53f841e4b233c00eba827a34ab57a5a6f
minjuice1 <minjuice93@gmail.com>
2022-04-25 16:18:44 +0900

    💄✨추가, 스크롤 적용하여 스크롤길이마다 프로그래스바 작동,

--- js-projects/case4-progress_bar/script.js
+++ js-projects/case4-progress_bar/script.js
@@ -4,4 +4,17 @@
   const get = (target) => {
     return document.querySelector(target)
   }
+
+  const $progressBar = get('.progress-bar')
+
+  const onScroll = () => {
+    const scrollTop = document.documentElement.scrollTop
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight
+    const widthScroll = (scrollTop / height) * 100
+    $progressBar.style.width = widthScroll + '%'
+  }
+
+  window.addEventListener('scroll', onScroll)
 })()
