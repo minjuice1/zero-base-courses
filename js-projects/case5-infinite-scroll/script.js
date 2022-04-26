2d0be83b57b83aef1cad3ceb32e70297a2e71af9
minjuice1 <minjuice93@gmail.com>
2022-04-26 18:45:48 +0900

    ✨추가, 무한스크롤 적용

--- js-projects/case5-infinite-scroll/script.js
+++ js-projects/case5-infinite-scroll/script.js
@@ -7,8 +7,13 @@
 
   const $posts = get('.posts')
 
+  const end = 100
+  const limit = 10
+  let total = 10
+  let page = 1
+
   const getPost = async () => {
-    const API_URL = 'https://jsonplaceholder.typicode.com/posts'
+    const API_URL = `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}`
     const res = await fetch(API_URL)
     if (!res.ok) {
       throw new Error('에러')
@@ -38,10 +43,24 @@
     showPosts(res)
   }
 
+  const onScroll = () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement
+    if (total === end) {
+      window.removeEventListener('scroll', onScroll)
+      return
+    }
+    if (scrollTop + clientHeight >= scrollHeight - 5) {
+      page++
+      total += 10
+      loadPost()
+    }
+  }
+
   const init = () => {
     window.addEventListener('DOMContentLoaded', () => {
       loadPost()
     })
+    window.addEventListener('scroll', onScroll)
   }
   init()
 })()
