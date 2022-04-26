119f783f33c213197f04cad4da5c6d27310140df
minjuice1 <minjuice93@gmail.com>
2022-04-26 23:05:43 +0900

    algorithm : 선형자료구조 - 수열 최솟값 위치

---
+++ algorithm/array/test1/배열문제풀이_수열최솟값위치_문제.js
@@ -0,0 +1,41 @@
+/* 수열 최소값 위치 */
+
+/* user code */
+function answer(nums) {
+	let result = [];
+
+	let min = 100;
+	for (let i = 0; i < nums.length; i++) {
+		if (min > nums[i]) {
+			min = nums[i];
+		}
+	}
+
+	let count = 0;
+	for (let i = 0; i < nums.length; i++) {
+		if (min == nums[i]) {
+			// result[count] = i;
+			// count++;
+			result.push(i);
+		}
+	}
+
+	return result;
+}
+
+/* main code */
+let input = [
+	// TC: 1
+	[5, 2, 10, 2],
+
+	// TC: 2
+	[4, 5, 7, 4, 8],
+
+	// TC: 3
+	[12, 11, 11, 16, 11, 12],
+];
+
+for (let i = 0; i < input.length; i++) {
+	process.stdout.write(`#${i + 1} `);
+	console.log(answer(input[i]));
+}
