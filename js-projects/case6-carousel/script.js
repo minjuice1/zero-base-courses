e72f542c32c731a7095a1dc9ab70f119f2cba5de
minjuice1 <minjuice93@gmail.com>
2022-04-28 18:33:29 +0900

    ✨추가, DOMContentLoaded 이벤트 생성, Carousel 클래스 생성

--- js-projects/case6-carousel/script.js
+++ js-projects/case6-carousel/script.js
@@ -4,4 +4,19 @@
   const get = (target) => {
     return document.querySelector(target)
   }
+
+  class Carousel {
+    constructor(carouselElement) {
+      this.carouselElement = carouselElement
+      this.itemClassName = 'carousel_item'
+      this.items = document.querySelectorAll('.carousel_item')
+      this.totalItems = this.items.length
+      this.current = 0
+    }
+  }
+
+  window.addEventListener('DOMContentLoaded', () => {
+    const carouselElement = get('.carousel')
+    const carousel = carousel(carouselElement)
+  })
 })()
