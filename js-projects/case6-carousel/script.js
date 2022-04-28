1553ef266cbfd91444ea1972e3fe5fd1796bc316
minjuice1 <minjuice93@gmail.com>
2022-04-28 19:03:25 +0900

    ✨ 추가, prev, next버튼 구현 및 무한루프 처리.

--- js-projects/case6-carousel/script.js
+++ js-projects/case6-carousel/script.js
@@ -14,15 +14,74 @@
       this.current = 0
     }
 
+    moveCarousel() {
+      let prev = this.current - 1
+      let next = this.current + 1
+
+      if (this.current === 0) {
+        prev = this.totalItems - 1
+      } else if (this.current === this.totalItems - 1) {
+        next = 0
+      }
+
+      for (let i = 0; i < this.totalItems; i++) {
+        if (i === this.current) {
+          this.items[i].className = this.itemClassName + ' active'
+        } else if (i === prev) {
+          this.items[i].className = this.itemClassName + ' prev'
+        } else if (i === next) {
+          this.items[i].className = this.itemClassName + ' next'
+        } else {
+          this.items[i].className = this.itemClassName
+        }
+      }
+    }
+
     initCarousel() {
-      this.items[this.items - 1].classList.add('prev')
+      this.items[this.totalItems - 1].classList.add('prev')
       this.items[0].classList.add('active')
       this.items[1].classList.add('next')
+    }
+
+    toPrev() {
+      if (this.current === 0) {
+        this.current = this.totalItems - 1
+      } else {
+        this.current--
+      }
+      this.moveCarousel()
+    }
+
+    toNext() {
+      if (this.current === this.totalItems - 1) {
+        this.current = 0
+      } else {
+        this.current++
+      }
+      this.moveCarousel()
+    }
+
+    setEventListener() {
+      this.prevButton = this.carouselElement.querySelector(
+        '.carousel_button--prev'
+      )
+      this.nextButton = this.carouselElement.querySelector(
+        '.carousel_button--next'
+      )
+
+      this.prevButton.addEventListener('click', () => {
+        this.toPrev()
+      })
+      this.nextButton.addEventListener('click', () => {
+        this.toNext()
+      })
     }
   }
 
   window.addEventListener('DOMContentLoaded', () => {
     const carouselElement = get('.carousel')
-    const carousel = carousel(carouselElement)
+    const carousel = new Carousel(carouselElement)
+    carousel.initCarousel()
+    carousel.setEventListener()
   })
 })()
