ac304e30905171366a063f2fb2a969206be8107e
minjuice1 <minjuice93@gmail.com>
2022-05-03 16:34:17 +0900

    algorithm : 연결리스트 구현1

---
+++ algorithm/LinkedList/LinkedList1.js
@@ -0,0 +1,26 @@
+function Node(data) {
+	this.data = data;
+	this.next = null;
+}
+
+function LinkedList() {
+	this.head = null;
+	this.length = 0;
+}
+
+LinkedList.prototype.size = function () {
+	return this.length;
+};
+
+LinkedList.prototype.isEmpty = function () {
+	return this.length === 0;
+};
+
+let ll = new LinkedList();
+ll.head = new Node(123);
+ll.length++;
+console.log(ll);
+
+ll.head.next = new Node(456);
+ll.length++;
+console.log(ll);
