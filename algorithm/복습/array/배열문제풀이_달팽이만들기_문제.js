34440872d38e49284c87e4d3c607b32d5e375a38
minjuice1 <minjuice93@gmail.com>
2022-05-19 14:33:31 +0900

    algorithm : 배열 문제 복습

---
+++ algorithm/복습/array/배열문제풀이_달팽이만들기_문제.js
@@ -0,0 +1,53 @@
+/* 달팽이 만들기 */
+
+/* user code */
+function answer(length) {
+	let result = [];
+
+	// 2차원 배열 반환
+	for (let i = 0; i < length; i++) {
+		result[i] = [];
+	}
+
+	let direction = 1;
+	let x = 0;
+	let y = 0;
+	let num = 0;
+
+	x--;
+	while (1) {
+		for (let i = 0; i < length; i++) {
+			x += direction;
+			result[y][x] = ++num;
+		}
+		length--;
+
+		if (length <= 0) break;
+
+		for (let j = 0; j < length; j++) {
+			y += direction;
+			result[y][x] = ++num;
+		}
+
+		direction *= -1;
+	}
+
+	return result;
+}
+
+/* main code */
+let input = [
+	// TC: 1
+	3,
+
+	// TC: 2
+	5,
+
+	// TC: 3
+	6,
+];
+
+for (let i = 0; i < input.length; i++) {
+	process.stdout.write(`#${i + 1} `);
+	console.log(answer(input[i]));
+}
---
+++ algorithm/복습/array/배열문제풀이_벽돌옮기기_문제.js
@@ -0,0 +1,36 @@
+/* 벽돌 옮기기 */
+
+/* user code */
+function answer(blocks) {
+	let result = 0;
+	let sum = 0;
+
+	for (let i = 0; i < blocks.length; i++) {
+		sum += blocks[i];
+	}
+	sum /= blocks.length;
+
+	for (let i = 0; i < blocks.length; i++) {
+		if (blocks[i] > sum) {
+			result += blocks[i] - sum;
+		}
+	}
+
+	return result;
+}
+
+/* main code */
+let input = [
+	// TC: 1
+	[5, 2, 4, 1, 7, 5],
+
+	// TC: 2
+	[12, 8, 10, 11, 9, 5, 8],
+
+	// TC: 3
+	[27, 14, 19, 11, 26, 25, 23, 15],
+];
+
+for (let i = 0; i < input.length; i++) {
+	console.log(`#${i + 1} ${answer(input[i])}`);
+}
