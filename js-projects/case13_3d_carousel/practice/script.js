83ec762aabc5697f581ca15b23aacb8e9dd22245
minjuice1 <minjuice93@gmail.com>
2022-07-09 22:39:52 +0900

    javascript : [add] 스타일링, prev&next 버튼구현

--- js-projects/case13_3d_carousel/practice/script.js
+++ js-projects/case13_3d_carousel/practice/script.js
@@ -4,4 +4,30 @@
   const get = (target) => {
     return document.querySelector(target)
   }
+
+  const getAll = (target) => {
+    return document.querySelectorAll(target)
+  }
+
+  const carousel = get('.carousel')
+  const $item = getAll('.item')
+  const cellCount = $item.length
+  let selectedIndex = 0
+
+  const rotateCarousel = () => {
+    const angle = (selectedIndex / cellCount) * -360
+    carousel.style.transform = 'translateZ(-346px) rotateY(' + angle + 'deg)'
+  }
+
+  const prevButton = get('.prev_button')
+  prevButton.addEventListener('click', () => {
+    selectedIndex--
+    rotateCarousel()
+  })
+
+  const nextButton = get('.next_button')
+  nextButton.addEventListener('click', () => {
+    selectedIndex++
+    rotateCarousel()
+  })
 })()
